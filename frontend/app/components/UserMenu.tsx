077a4efb50a5ab5d65867fa0c8d499e195825450
lokytech5 <lokytech5@gmail.com>
2024-08-05 19:07:43 +0100

    Add notification bell to usermenu and change product icon in drawer

--- frontend/app/components/UserMenu.tsx
+++ frontend/app/components/UserMenu.tsx
@@ -1,26 +1,30 @@
+import Image from "next/image";
 import React from "react";
+import { MdNotificationsActive } from "react-icons/md";
+
 
 const UserMenu = () => {
   return (
-    <div className="navbar-end flex items-center space-x-4">
+    <div className="navbar-end flex items-center space-x-3">
       <div className="dropdown dropdown-end">
         <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
           <div className="indicator">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-              />
-            </svg>
-            <span className="badge badge-sm indicator-item">8</span>
+          <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+                />
+              </svg>
+              <span className="badge badge-xs badge-error indicator-item"></span>
+           
           </div>
         </div>
         <div
@@ -31,7 +35,7 @@ const UserMenu = () => {
             <span className="font-bold text-lg">8 Items</span>
             <span className="text-info">Subtotal: $999</span>
             <div className="card-actions">
-              <button className="btn btn-primary btn-block">View cart</button>
+              <button className="btn btn-primary btn-block">View</button>
             </div>
           </div>
         </div>
@@ -43,9 +47,12 @@ const UserMenu = () => {
           className="btn btn-ghost btn-circle avatar"
         >
           <div className="w-10 rounded-full">
-            <img
+            <Image
               alt="Profile"
               src="/images/avatar.svg"
+              width={40}
+              height={40}
+              className="rounded-full"
             />
           </div>
         </div>
