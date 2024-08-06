2283f55d300ee496e8a1e5fbff4511e1b2c21e2a
lokytech5 <lokytech5@gmail.com>
2024-08-06 19:26:06 +0100

    add a simple get request to test deplyoment

--- backend/index.ts
+++ backend/index.ts
@@ -2,6 +2,10 @@ import express from 'express';
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send("Hello, world!");
+})
+
 app.listen(8000, () => {
     console.log("started listening to server on port " + 8000);
     
